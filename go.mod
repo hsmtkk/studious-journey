e8b714c091d8a9489de8b7339a401123d9a75be9
hsmtkk <hsmtkk@gmail.com>
2022-11-22 07:19:30 +0900

    issue custom metrics

--- go.mod
+++ go.mod
@@ -2,18 +2,36 @@ module github.com/hsmtkk/studious-journey
 
 go 1.19
 
-require github.com/labstack/echo/v4 v4.9.1
+require (
+	cloud.google.com/go/monitoring v1.9.0
+	github.com/labstack/echo/v4 v4.9.1
+	golang.org/x/oauth2 v0.0.0-20221014153046-6fdb5e3db783
+	google.golang.org/api v0.103.0
+)
 
 require (
+	cloud.google.com/go/compute v1.12.1 // indirect
+	cloud.google.com/go/compute/metadata v0.2.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
+	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
-	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/net v0.0.0-20221014081412-f15817d10f9b // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	golang.org/x/text v0.4.0 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20221109142239-94d6d90a7d66 // indirect
+	google.golang.org/grpc v1.50.1 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
--- web/web.go
+++ web/web.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	monitoring "cloud.google.com/go/monitoring/apiv3"
+	"cloud.google.com/go/monitoring/apiv3/v2/monitoringpb"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/compute/v1"
 )
 
 func main() {
@@ -14,6 +20,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	hdl := newHandler()
 
 	// Echo instance
 	e := echo.New()
@@ -23,13 +31,62 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.GET("/", hello)
+	e.GET("/", hdl.index)
 
 	// Start server
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", port)))
 }
 
+type handler struct{}
+
+func newHandler() *handler {
+
+	return &handler{}
+}
+
 // Handler
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+func (h *handler) index(ectx echo.Context) error {
+	ctx := ectx.Request().Context()
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://www.example.com", nil)
+	if err != nil {
+		return fmt.Errorf("http.NewRequestWithContext failed; %w", err)
+	}
+	before := time.Now()
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("http.DefaultClient.Do failed; %w", err)
+	}
+	defer resp.Body.Close()
+	elapsed := time.Since(before)
+	if err := h.recordMetrics(ctx, elapsed); err != nil {
+		return err
+	}
+	return ectx.String(http.StatusOK, "Hello, World!")
+}
+
+func (h *handler) recordMetrics(ctx context.Context, elapsed time.Duration) error {
+	projectID, err := h.projectID(ctx)
+	if err != nil {
+		return err
+	}
+	clt, err := monitoring.NewMetricClient(ctx)
+	if err != nil {
+		return fmt.Errorf("monitoring.NewMetricClient failed; %w", err)
+	}
+	defer clt.Close()
+	req := monitoringpb.CreateTimeSeriesRequest{
+		Name: "projects/" + projectID,
+	}
+	if err := clt.CreateTimeSeries(ctx, &req); err != nil {
+		return fmt.Errorf("monitoring.MetricClient.CreateTimeSeries failed; %w", err)
+	}
+	return nil
+}
+
+func (h *handler) projectID(ctx context.Context) (string, error) {
+	credentials, err := google.FindDefaultCredentials(ctx, compute.ComputeScope)
+	if err != nil {
+		return "", fmt.Errorf("google.FindDefaultCredentials failed; %w", err)
+	}
+	return credentials.ProjectID, nil
 }
